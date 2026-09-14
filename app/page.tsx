52befa565c7e1ab4c86cfbcf885995bc6a2a2da1
shuvinexofficial143-design <shuvinex.official.143@gmail.com>
2026-09-14 11:52:54 +0530

    Add xKiro story model selector to main continuity studio

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import {AnalysisModelSelector} from "@/components/analysis-model-selector";
 import {CinematicContinuityStudio} from "@/components/cinematic-continuity-studio";
 
 export default function HomePage(){
-  return <CinematicContinuityStudio/>;
+  return <>
+    <AnalysisModelSelector/>
+    <CinematicContinuityStudio/>
+  </>;
 }
