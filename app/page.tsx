fa6bff2e742a619c031a254cd482ac0e9be7faee
shuvinexofficial143-design <shuvinex.official.143@gmail.com>
2026-09-19 12:20:36 +0530

    refactor: render Manga Studio persistently from app shell

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
-import {AnalysisModelSelector} from "@/components/analysis-model-selector";
-import {StoryFrameWorkspace} from "@/components/storyframe-workspace";
-
 export const dynamic="force-dynamic";
 
 export default function HomePage(){
-  return <>
-    <AnalysisModelSelector/>
-    <StoryFrameWorkspace/>
-  </>;
+  return null;
 }
